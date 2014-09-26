52d17e020171d12b4e1b7c1c63a6cd036a23facf
snowonsummer <yukie@snowonsummer.com>
2014-09-26 09:59:18 +0700

    Wow, first check

---
+++ javaclock.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+	<title>Algorithm Test</title>
+</head>
+<body onload="onloadFunctions();">
+<div id="aux"></div>
+
+<script type="text/javascript">
+function onloadFunctions() {
+	var count = 0;
+	var refId = null;
+	var target = document.getElementById("aux"),
+		n;
+
+	var countUp = function() {
+		n = new Date();
+		t = n.getDate() + "-" + n.getMonth() + "-" + n.getFullYear() + " " + n.getHours() + ":" + n.getMinutes() + ":" + n.getSeconds();
+		target.innerHTML = t;
+	}
+
+	refId = window.setInterval(countUp, 1000);
+}
+</script>
+</body>
+</html>
